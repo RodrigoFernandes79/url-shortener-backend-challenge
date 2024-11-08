4a01788d849fc37be36fbbc25db35557b0f12f98
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-11-08 20:01:31 -0300

    feat: add UrlEntity class for URL data model

---
+++ src/main/java/com/tds/url_shortener/domain/entity/UrlEntity.java
@@ -0,0 +1,75 @@
+package com.tds.url_shortener.domain.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Document(collection = "urls")
+public class UrlEntity {
+    @Id
+    private String id;
+    private String originalUrl;
+    private String shortUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastAccessed;
+    private int accessCount = 0;
+
+    public UrlEntity() {
+    }
+
+    public UrlEntity(String id, String originalUrl, String shortUrl) {
+        this.id = id;
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getLastAccessed() {
+        return lastAccessed;
+    }
+
+    public void setLastAccessed(LocalDateTime lastAccessed) {
+        this.lastAccessed = lastAccessed;
+    }
+
+    public int getAccessCount() {
+        return accessCount;
+    }
+
+    public void setAccessCount(int accessCount) {
+        this.accessCount = accessCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UrlEntity urlEntity = (UrlEntity) o;
+        return Objects.equals(id, urlEntity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
