0c972999b6f6d4bfc2eec26b4f38f033fdda8e86
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-11-09 20:36:59 -0300

    feat: add custom query findByOriginalUrl to UrlEntityRepository

--- src/main/java/com/tds/url_shortener/repository/UrlEntityRepository.java
+++ src/main/java/com/tds/url_shortener/repository/UrlEntityRepository.java
@@ -3,5 +3,8 @@ package com.tds.url_shortener.repository;
 import com.tds.url_shortener.domain.entity.UrlEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UrlEntityRepository extends MongoRepository<UrlEntity,String> {
+import java.util.Optional;
+
+public interface UrlEntityRepository extends MongoRepository<UrlEntity, String> {
+    Optional<UrlEntity> findByOriginalUrl(String originalUrl);
 }
