e79a1292b2d7c74d9689d68f25444584ffbbe76e
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-11-09 20:33:11 -0300

    feat: add UrlService with shortenUrl method for generating short URLs

---
+++ src/main/java/com/tds/url_shortener/service/UrlService.java
@@ -0,0 +1,35 @@
+package com.tds.url_shortener.service;
+
+import com.tds.url_shortener.domain.dto.ShortenUrlResponseDto;
+import com.tds.url_shortener.domain.dto.UrlRequestDto;
+import com.tds.url_shortener.domain.entity.UrlEntity;
+import com.tds.url_shortener.exceptions.OriginalUrlFoundException;
+import com.tds.url_shortener.exceptions.UrlNotProvidedException;
+import com.tds.url_shortener.repository.UrlEntityRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlService {
+    @Autowired
+    private UrlEntityRepository urlRepository;
+
+    public ShortenUrlResponseDto shortenUrl(UrlRequestDto urlRequest, HttpServletRequest httpServletRequest) {
+        if (urlRequest.originalUrl() == null || urlRequest.originalUrl().trim().isEmpty()) {
+            throw new UrlNotProvidedException("Original Url is not provided");
+        }
+        var urlFound = urlRepository.findByOriginalUrl(urlRequest.originalUrl());
+        if (urlFound.isPresent()) {
+            throw new OriginalUrlFoundException("Original Url Already exists");
+        }
+        String id;
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
+        } while (urlRepository.existsById(id));
+        var redirectUrl = httpServletRequest.getRequestURL().toString().replace("shorten", id);
+        urlRepository.save(new UrlEntity(id, urlRequest.originalUrl(), redirectUrl));
+        return new ShortenUrlResponseDto(redirectUrl);
+    }
+}
