c991bab5f28dcef4da8ba985e104bbf0b37d4d00
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-11-10 17:50:07 -0300

    feat: handle validation exceptions and return custom error messages

--- src/main/java/com/tds/url_shortener/exceptions/GlobalExceptionsHandler.java
+++ src/main/java/com/tds/url_shortener/exceptions/GlobalExceptionsHandler.java
@@ -2,10 +2,13 @@ package com.tds.url_shortener.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionsHandler {
@@ -27,5 +30,18 @@ public class GlobalExceptionsHandler {
                 LocalDateTime.now()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DataFieldValidation>> dataField(MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors();
+        var error = errors.stream().map(DataFieldValidation::new).toList();
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    private record DataFieldValidation(String error, String message) {
+        private DataFieldValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
